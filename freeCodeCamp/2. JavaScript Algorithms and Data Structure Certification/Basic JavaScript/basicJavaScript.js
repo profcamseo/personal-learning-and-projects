2e96272d056ae71dd60b43de5953867de926f448
profcamseo <cam.w.milner@gmail.com>
2020-05-08 16:51:41 +1000

    daily learning 20200508

---
+++ freeCodeCamp/2. JavaScript Algorithms and Data Structure Certification/Basic JavaScript/basicJavaScript.js
@@ -0,0 +1,61 @@
+/*============================
+Comment Your JavaScript Code
+============================*/
+
+
+// This is an in-line comment.
+/* This is a
+multi-line comment */
+
+/*============================
+Declare JavaScript Variables
+============================*/
+
+/* Data Types:
+undefined, null, boolean, string, symbol, number, and object
+*/
+
+// Example
+var ourName;
+
+// Declare myName below this line
+var myName;
+myName = "Cameron";
+
+/*============================
+Storing Values with the Assignment Operator
+============================*/
+
+// Setup
+var a;
+var b = 2;
+
+// Only change code below this line
+a = 7;
+b = 7;
+b = a;
+
+/*============================
+Initializing Variables with the Assignment Operator
+============================*/
+
+// Example
+var ourVar = 19;
+
+// Only change code below this line
+var a = 9;
+
+/*============================
+Understanding Uninitialized Variables
+============================*/
+
+// Initialize these three variables
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+// Do not change code below this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
