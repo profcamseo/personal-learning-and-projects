493833221ca14928d8bb75ab050c1c6e4ca532c9
profcamseo <cam.w.milner@gmail.com>
2020-05-11 14:28:48 +1000

    lunch learning 20200511

--- freeCodeCamp/2. JavaScript Algorithms and Data Structure Certification/Basic JavaScript/basicJavaScript.js
+++ freeCodeCamp/2. JavaScript Algorithms and Data Structure Certification/Basic JavaScript/basicJavaScript.js
@@ -59,3 +59,45 @@ var c = "I am a";
 a = a + 1;
 b = b + 5;
 c = c + " String!";
+
+/*============================
+Understanding Case Sensitivity in Variable
+============================*/
+
+// Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+/*============================
+Add Two Numbers with JavaScript
+============================*/
+
+var sum = 10 + 10;
+
+/*============================
+Subtract One Number from Another with JavaScript
+============================*/
+
+var difference = 45 - 33;
+
+
+
+/*============================
+Multiply Two Numbers with JavaScript
+============================*/
+
+
+var product = 8 * 10;
+
+/*============================
+Divide One Number by Another with JavaScript
+============================*/
+
+var quotient = 66 / 33;
+
