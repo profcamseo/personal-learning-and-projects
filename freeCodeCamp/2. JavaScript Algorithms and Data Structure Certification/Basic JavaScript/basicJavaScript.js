c35c22f5d1aa3643028a79a281c22580f55a32a0
profcamseo <cam.w.milner@gmail.com>
2020-06-04 17:44:48 +1000

    daily learning 20200604

--- freeCodeCamp/2. JavaScript Algorithms and Data Structure Certification/Basic JavaScript/basicJavaScript.js
+++ freeCodeCamp/2. JavaScript Algorithms and Data Structure Certification/Basic JavaScript/basicJavaScript.js
@@ -505,3 +505,287 @@ var myArray = [
 
 // Only change code below this line.
 var removedFromMyArray = myArray.shift();
+
+/*============================
+Manipulate Arrays With unshift()
+============================*/
+
+// Example
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.shift(); // ourArray now equals ["J", "cat"]
+ourArray.unshift("Happy");
+// ourArray now equals ["Happy", "J", "cat"]
+
+// Setup
+var myArray = [
+  ["John", 23],
+  ["dog", 3],
+];
+myArray.shift();
+
+// Only change code below this line.
+myArray.unshift(["Paul", 35]);
+
+/*============================
+Shopping List
+============================*/
+
+var myList = [
+  ["Chocolate Bar", 15],
+  ["Chocolate Bar", 15],
+  ["Chocolate Bar", 15],
+  ["Chocolate Bar", 15],
+  ["Chocolate Bar", 15],
+];
+
+/*============================
+Write Reusable JavaScript with Functions
+============================*/
+
+// Example
+function ourReusableFunction() {
+  console.log("Heyya, World");
+}
+
+ourReusableFunction();
+
+// Only change code below this line
+
+function reusableFunction() {
+  console.log("Hi World");
+}
+
+reusableFunction();
+
+/*============================
+Passing Values to Functions with Arguments
+============================*/
+
+// Example
+function ourFunctionWithArgs(a, b) {
+  console.log(a - b);
+}
+ourFunctionWithArgs(10, 5); // Outputs 5
+
+// Only change code below this line.
+function functionWithArgs(a, b) {
+  console.log(a + b);
+}
+functionWithArgs(10, 5);
+
+/*============================
+Global Scope and Functions
+============================*/
+
+// Declare your variable here
+var myGlobal = 10;
+
+function fun1() {
+  // Assign 5 to oopsGlobal Here
+  oopsGlobal = 5;
+}
+
+// Only change code above this line
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
+
+/*============================
+Local Scope and Functions
+============================*/
+
+function myLocalScope() {
+  "use strict"; // you shouldn't need to edit this line
+  var myVar = 5;
+  console.log(myVar);
+}
+myLocalScope();
+
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+
+// Now remove the console log line to pass the test
+
+/*============================
+Global vs. Local Scope in Functions
+============================*/
+
+// Setup
+var outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+  var outerWear = "sweater";
+
+  // Only change code above this line
+  return outerWear;
+}
+
+console.log(myOutfit());
+console.log(outerWear);
+
+/*============================
+Return a Value from a Function with Return
+============================*/
+
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+
+function timesFive(num) {
+  return num * 5;
+}
+
+console.log(minusSeven(10));
+
+/*============================
+Understanding Undefined Value returned from a Function
+============================*/
+
+// Example
+var sum = 0;
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+
+// Only change code above this line
+var sum = 8;
+
+function addFive() {
+  sum += 5;
+}
+
+/*============================
+Assignment with a Returned Value
+============================*/
+
+// Example
+var changed = 0;
+
+function change(num) {
+  return (num + 5) / 3;
+}
+
+changed = change(10);
+
+// Setup
+var processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+// Only change code below this line
+
+processed = processArg(7); //this number becomes (num) in above formula
+
+/*============================
+Stand in Line
+============================*/
+
+function nextInLine(arr, item) {
+  // Your code here
+  arr.push(item);
+  return arr.shift(); // Change this line
+}
+
+// Test Setup
+var testArr = [1, 2, 3, 4, 5];
+
+// Display Code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6)); // Modify this line to test
+console.log("After: " + JSON.stringify(testArr));
+
+//need to study this, don't understand
+
+/*============================
+Understanding Boolean Values
+============================*/
+
+function welcomeToBooleans() {
+  // Only change code below this line.
+
+  return true; // Change this line
+
+  // Only change code above this line.
+}
+
+/*============================
+Use Conditional Logic with If Statements
+============================*/
+
+// Example
+function ourTrueOrFalse(isItTrue) {
+  if (isItTrue) {
+    return "Yes, it's true";
+  }
+  return "No, it's false";
+}
+
+// Setup
+function trueOrFalse(wasThatTrue) {
+  // Only change code below this line.
+
+  if (wasThatTrue) {
+    return "Yes, that was true";
+  }
+  // Only change code above this line.
+  return "No, that was false";
+}
+
+// Change this value to test
+trueOrFalse(true);
+
+/*============================
+Comparison with the Equality Operator
+============================*/
+
+// Setup
+function testEqual(val) {
+  if (val == 12) {
+    // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+testEqual(12);
+
+10 == 10;
+10 == 12;
+12 == "12";
+"12" == 12;
+
+/*============================
+Comparison with the Strict Equality Operator
+============================*/
+
+// Setup
+function testStrict(val) {
+  if (val === 7) {
+    // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+testStrict(7);
+
+7 === 10;
+7 === 7;
+7 === "7";
